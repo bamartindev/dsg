838d2cc18c11c9641126e87bae450cb9caddf876
Brett Martin <bamartindev@gmail.com>
2021-09-30 19:36:29 -0500

    Removed code from Main function that was added as aa temporary test before the unit tests were added

--- app/Main.hs
+++ app/Main.hs
@@ -1,19 +1,6 @@
 module Main where
-import DSG.Domain.Workspace (mkWorkspace, addTaskToBacklog)
-import DSG.Domain.Task (mkTask)
 
 main :: IO ()
 main = do 
   putStrLn "Daily Standup Generator (DSG) v0.1.0"
-  ws <- mkWorkspace "Test Workspace"
-  first <- mkTask "first task"
-  second <- mkTask "second task"
-  third <- mkTask "third task"
-
-  -- adding in the order second, first, third to make sure the sorting is also working
-  let result = addTaskToBacklog third 
-                $ addTaskToBacklog first 
-                $ addTaskToBacklog second ws
-
-  print result
         
